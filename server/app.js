2231f5c2f8ebd834df760f95c1cb619190395e1a
Taiyaba Khan <146852439+Taiyabakhan@users.noreply.github.com>
2025-03-30 11:41:42 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // Initialize Web3
 const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-
+//const web3 = new Web3(Web3.givenProvider || 'http://127.0.0.1:8545');
 // Initialize IPFS
 const ipfs = create({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
