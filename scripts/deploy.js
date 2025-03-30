72bd4bca26b311f4f610acff44c480c9b35403ab
Taiyaba Khan <146852439+Taiyabakhan@users.noreply.github.com>
2025-03-30 11:40:36 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,21 +2,28 @@ const fs = require('fs');
 const Web3 = require('web3').default;
 const { interface, bytecode } = require('./compile');
 
-const web3 = new Web3(('http://localhost:8545'));
+const web3 = new Web3('http://localhost:8545');
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
-  console.log('Attempting to deploy from account', accounts[0]);
+  try {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+    const contract = new web3.eth.Contract(interface);
 
-  console.log('Contract deployed to', result.options.address);
-  fs.writeFileSync(
-    '../client/src/contractAddress.js',
-    `export const contractAddress = '${result.options.address}'`
-  );
+    const result = await contract
+      .deploy({ data: bytecode })
+      .send({ gas: '1000000', from: accounts[0], gasPrice: '20000000000' }); // Set gasPrice explicitly
+
+    console.log('Contract deployed to', result.options.address);
+
+    fs.writeFileSync(
+      '../client/src/contractAddress.js',
+      `export const contractAddress = '${result.options.address}';`
+    );
+  } catch (error) {
+    console.error('Deployment failed:', error);
+  }
 };
 
 deploy();
